2f8b058601e7f8608976797754785b4a27d91873
rock3tsprocket <rock-pie@hotmail.com>
2025-12-31 21:58:44 +0100

    make libcrazy ANSI C-compliant

--- libcrazy.c
+++ libcrazy.c
@@ -29,6 +29,9 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 #include <stdlib.h>
 #include <string.h>
 
+/* Comply with ANSI C */
+int i;
+
 long int dectotern(long int tobeterned) {
 	/* Conversion to ternary */
 	int remainder;
@@ -88,7 +91,7 @@ long int crazy(long int tobecrazied1, long int tobecrazied2, bool areintstern, b
 	 * https://en.wikipedia.org/wiki/Malbolge#Crazy_operation */
 	size_t length = strlen(crazied1) > strlen(crazied2) ? strlen(crazied1) : strlen(crazied2);
 
-	for (int i = 0; i < (int) length; i++) {
+	for (i = 0; i < (int) length; i++) {
 		trit1 = crazied1[i] - '0';
 		trit2 = crazied2[i] - '0';
 		int tritresult = 0;
